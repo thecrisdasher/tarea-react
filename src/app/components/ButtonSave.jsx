bfcf1407e80f7fe87f343843ca63785220547ee7
thecrisdasher <dcristian642@gmail.com>
2025-02-25 16:04:53 -0500

    tarea-react-with-nextjs

---
+++ src/app/components/ButtonSave.jsx
@@ -0,0 +1,8 @@
+let BottonSave = () => {
+    return (
+        <button className="bg-[#1DA1F2] text-white py-2 px-4 rounded-full">
+            Guardar
+        </button>
+    )
+}
+export default BottonSave;
